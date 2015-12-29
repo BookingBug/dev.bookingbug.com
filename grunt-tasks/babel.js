d2ef6f62cbd421434949a7903d31ea2cdb1fded4
Max McKenzie <max@lemondigits.com>
2015-12-29 15:27:54 +0000

    added in bable so that you can now right in ecma script 6

---
+++ grunt-tasks/babel.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt, options) {
+
+   var fs_files = grunt.file.expand("assets/javascript/**/*.js");
+   var files = {};
+   for(var file in fs_files) {
+      file = fs_files[file];
+      files["tmp/babel/" + file] = file;
+   }
+
+   return {
+      options: {
+         sourceMap: true,
+         presets: ['es2015']
+      },
+      dist: {
+         files: files
+      }
+   };
+
+};
