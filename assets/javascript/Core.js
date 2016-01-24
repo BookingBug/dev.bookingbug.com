89fbd6103ff840b908606229df09e24d5fd554c3
Max McKenzie <max@lemondigits.com>
2016-01-24 14:38:33 +0000

    desktop notification support and basic es6 core file

--- assets/javascript/Core.js
+++ assets/javascript/Core.js
@@ -1,7 +1,34 @@
 "use strict";
 
-class yourClass {
-	contructor() {
-		console.log('loaded Core Javascript');
+class Core {
+	constructor() {
+		this.log('Loaded Core JavaScript');
+		this.checkPerms();
+		this.notify('Notifications Enabled!', 'FrontEndSeed comes with Desktop Notifications');
 	}
+
+	checkPerms() {
+		if(Notification.permission !== 'granted'){
+			Notification.requestPermission();
+			this.log('Need desktop notification permission');
+		} else {
+			this.log('Desktop notification permission granted');
+		}
+	}
+
+	notify(title, message) {
+		var n;
+		n = new Notification( title, {
+			body: message, 
+			icon : ""
+		});
+		setTimeout(function(){ n.close(); }, 4000);
+	}
+
+	log(message) {
+		console.log(message);
+	}
+
 }
+
+new Core();
