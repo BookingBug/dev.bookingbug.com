3d742f24513756b0ca6511b3a745240c42842845
Max McKenzie <max@lemondigits.com>
2016-02-17 15:39:29 +0000

    finished disqus intergration

--- assets/javascript/Core.js
+++ assets/javascript/Core.js
@@ -4,43 +4,15 @@ class Core {
 	constructor() {
 		this.log('Loaded Core JavaScript');
 		this.menuLinks();
-		this.checkDependancies();
 
 		const $toggle = $('.menu-toggle');
 		const $target = $('.main-menu');
 
-		this.bindMenu($target, $toggle);
-		//TODO if url matches menu item href add class active
+		this.menuBind($target, $toggle);
 		
-		// this.checkPerms();
-		// this.notify('Notifications Enabled!', 'FrontEndSeed comes with Desktop Notifications');
-	}
-
-	checkPerms() {
-		if(Notification.permission !== 'granted'){
-			Notification.requestPermission();
-			this.log('Need desktop notification permission');
-		} else {
-			this.log('Desktop notification permission granted');
-		}
-	}
-
-	checkDependancies() {
-		// Check for the various File API support.
-		if (window.File && window.FileReader && window.FileList && window.Blob) {
-		// Great success! All the File APIs are supported.
-		} else {
-			alert('The File APIs are not fully supported in this browser.');
-		}
-	}
-
-	notify(title, message) {
-		var n;
-		n = new Notification( title, {
-			body: message, 
-			icon : ""
-		});
-		setTimeout(function(){ n.close(); }, 4000);
+		this.disqusLoad(window.location['href'], window.location.pathname);
+		
+		// TODO if url matches menu item href add class active  to said menu item
 	}
 
 	log(message) {
@@ -48,12 +20,10 @@ class Core {
 	}
 
 	menuLinks() {
-		this.log('loaded bind');
 		const $menuItem = $('nav.main-menu a');
 
 		$menuItem.each((index, value) => {
 			$(value).on('click', event => {
-				this.log('sdf')
 				if ($(value).attr('target')) {
 					return
 				} else {
@@ -74,14 +44,17 @@ class Core {
 	switchContent(path) {
 		this.log(path)
 		$('.content > .container').fadeOut(() => {
+			this.disqusHide();
 			$('.content > .container').load( path + ' .content > .container',() => {
-				$('.content > .container').fadeIn();
+				$('.content > .container').fadeIn(() => {
+					this.disqusReload(path);
+				});
 				$('body').animate({scrollTop:0}, '500', 'swing');
 			});
 		});
 	}
 
-	bindMenu($target, $toggle) {
+	menuBind($target, $toggle) {
 		$toggle.on('click', (e) => {
 			if ($target.hasClass('active')) {
 				this.menuClose($target, $toggle);
@@ -101,6 +74,43 @@ class Core {
 		$toggle.removeClass('active');
 	}
 
+	disqusLoad(url, path) {
+		var disqus_config = function () {
+			this.page.url = url;
+			this.page.identifier = path;
+		};
+		this.disqusBuildDom();
+	}
+
+	disqusReload(path) {
+		this.disqusBuildDom(() => {
+			DISQUS.reset({
+				reload: true,
+				config: function () {  
+					this.page.identifier = path;  
+					this.page.url =  window.location.protocol + '://' + window.location.host + path;
+				}
+			});
+		});
+	}
+
+	disqusBuildDom() {
+		var d = document, s = d.createElement('script');
+		s.src = '//devdocsbookingbug.disqus.com/embed.js';
+		s.setAttribute('data-timestamp', +new Date());
+		(d.head || d.body).appendChild(s);
+		this.disqusShow()
+	}
+
+	disqusShow() {
+		const $comments = $('.comments');
+		$comments.fadeIn(500);
+	}
+
+	disqusHide() {
+		const $comments = $('.comments');
+		$comments.hide();
+	}
 }
 
 new Core();
