0964b1a0278883879241ecd338efaf65fc9e0f65
Max McKenzie <max@lemondigits.com>
2016-03-23 12:10:30 +0000

    standardised the grunt.js file inline with airbnb style guide

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,27 @@
-var path = require('path');
-var LoadGruntConfig = require('load-grunt-config');
-var execSync = require('child_process').execSync;
+const path = require('path');
+const LoadGruntConfig = require('load-grunt-config');
+const execSync = require('child_process').execSync;
 
 module.exports = function(grunt) {
-    var environment = (process.env.LD_ENVIRONMENT || "local");
+  const environment = (process.env.LD_ENVIRONMENT || 'local');
 
-    var localGitHash = null;
-    try {
-        localGitHash = execSync("git rev-parse HEAD").toString().replace(/(\r\n|\n|\r)/gm, "");
-    } catch(e) { }
+  const localGitHash = null;
+  try {
+    localGitHash = execSync('git rev-parse HEAD').toString().replace(/(\r\n|\n|\r)/gm, '');
+  } catch (e) {
+    return;
+  }
 
-    var project_config = {};
-    project_config.__versionString = (process.env.CI_BUILD_REF || localGitHash || "ffffff").substr(0, 6);
-    project_config.environment     = environment;
+  const project_config = {};
+  project_config.__versionString = (
+    process.env.CI_BUILD_REF || localGitHash || 'ffffff'
+  ).substr(0, 6);
+  project_config.environment = environment;
 
-    require('time-grunt')(grunt);
+  require('time-grunt')(grunt);
 
-    LoadGruntConfig(grunt, {
-        configPath: path.join(process.cwd(), 'grunt-tasks'),
-        data: project_config
-    });
+  LoadGruntConfig(grunt, {
+    configPath: path.join(process.cwd(), 'grunt-tasks'),
+    data: project_config,
+  });
 };
