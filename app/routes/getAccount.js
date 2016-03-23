41d0d3ba548fa1609a00825055084cee1d9303e3
Max McKenzie <max@lemondigits.com>
2016-03-23 17:19:52 +0000

    pull session user variables into template

--- app/routes/getAccount.js
+++ app/routes/getAccount.js
@@ -2,6 +2,11 @@ import Page from '../classes/Page';
 
 export default (req, res) => {
   const templatePath = '/templates/account.twig';
-  const page = new Page({ templatePath });
+  const page = new Page({
+    templatePath,
+    templateVariables: {
+      user: req.session,
+    },
+  });
   res.send(page.render());
 };
