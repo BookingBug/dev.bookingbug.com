6c142a4b22c8104e58dd52d58b6c2822bedee6be
Max McKenzie <max@lemondigits.com>
2016-03-23 17:23:15 +0000

    added 403 error code if no user session is set

--- app/routes/getAccount.js
+++ app/routes/getAccount.js
@@ -1,12 +1,17 @@
 import Page from '../classes/Page';
 
 export default (req, res) => {
-  const templatePath = '/templates/account.twig';
-  const page = new Page({
-    templatePath,
-    templateVariables: {
-      user: req.session,
-    },
-  });
-  res.send(page.render());
+  if (req.session) {
+    const templatePath = '/templates/account.twig';
+    const page = new Page({
+      templatePath,
+      templateVariables: {
+        user: req.session,
+      },
+    });
+    res.send(page.render());
+  } else {
+    return res.sendStatus(403);
+  }
+  return true;
 };
