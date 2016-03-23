23ee61255fc2654885f3d8211fc11fa779f085c4
Max McKenzie <max@lemondigits.com>
2016-03-23 14:50:56 +0000

    refactored loaction of assets and added account template code

--- app/index.js
+++ app/index.js
@@ -6,6 +6,7 @@ import getDocsRoute from './routes/getDocs';
 import postLoginRoute from './routes/postLogin';
 import getLoginRoute from './routes/getLogin';
 import getRegisterRoute from './routes/getRegister';
+import getAccountRoute from './routes/getAccount';
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.get('/docs/:category/:page?', getDocsRoute);
 app.get('/login', getLoginRoute);
 app.post('/login', postLoginRoute);
 app.get('/register', getRegisterRoute);
+app.get('/account', getAccountRoute);
 
 app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
