d45561ef09a7eeafd6377ab1927191e313ece330
Max McKenzie <max@lemondigits.com>
2016-03-23 11:13:21 +0000

    started port into apps folder with express routes

--- entrypoint.js
+++ entrypoint.js
@@ -1,8 +1,13 @@
-'use strict'
+// console.log('loaded entrypoint.js');
+// require('babel-register');
 
-console.log('loaded entrypoint.js');
-require('babel-register');
+// let PortalRoutes = require('./src/Routes.js');
 
-let PortalRoutes = require('./src/Routes.js');
+// new PortalRoutes();
 
-new PortalRoutes();
+if (process.env.NODE_ENV === 'development') {
+  require('babel-register');
+  require('./app/index');
+} else {
+  require('./dist/index');
+}
