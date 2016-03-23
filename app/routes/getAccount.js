78dd4990705c9f3773b3160a929ff76b38ce9c9e
Max McKenzie <max@lemondigits.com>
2016-03-23 17:24:20 +0000

    removed error code awaiting review on auth and error handling

--- app/routes/getAccount.js
+++ app/routes/getAccount.js
@@ -1,17 +1,12 @@
 import Page from '../classes/Page';
 
 export default (req, res) => {
-  if (req.session) {
-    const templatePath = '/templates/account.twig';
-    const page = new Page({
-      templatePath,
-      templateVariables: {
-        user: req.session,
-      },
-    });
-    res.send(page.render());
-  } else {
-    return res.sendStatus(403);
-  }
-  return true;
+  const templatePath = '/templates/account.twig';
+  const page = new Page({
+    templatePath,
+    templateVariables: {
+      user: req.session,
+    },
+  });
+  res.send(page.render());
 };
