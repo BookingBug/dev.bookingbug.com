336a85797c2c076834b2d4a5df8bc8395dcf9dbe
Max McKenzie <max@lemondigits.com>
2016-04-13 13:12:49 +0100

    removed grunt config variables to set up dynamic templates

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,20 +5,6 @@ const loadGruntConfig = require('load-grunt-config');
 const execSync = require('child_process').execSync;
 
 module.exports = function (grunt) {
-  const environment = (process.env.LD_ENVIRONMENT || 'local');
-
-  let localGitHash = null;
-  try {
-    localGitHash = execSync('git rev-parse HEAD').toString().replace(/(\r\n|\n|\r)/gm, '');
-  } catch (e) {
-    return;
-  }
-
-  const projectConfig = {};
-  projectConfig.__versionString = (
-    process.env.CI_BUILD_REF || localGitHash || 'ffffff'
-  ).substr(0, 6);
-  projectConfig.environment = environment;
 
   require('time-grunt')(grunt);
 
@@ -26,6 +12,5 @@ module.exports = function (grunt) {
 
   loadGruntConfig(grunt, {
     configPath: path.join(process.cwd(), '/grunt-tasks'),
-    data: projectConfig,
   });
 };
