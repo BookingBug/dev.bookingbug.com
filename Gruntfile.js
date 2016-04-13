409d7eb7b28653130259ecc568a347f805a1d105
Max McKenzie <max@lemondigits.com>
2016-04-13 13:08:26 +0100

    added console log and amended the configPath of grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,8 +22,10 @@ module.exports = function (grunt) {
 
   require('time-grunt')(grunt);
 
+  console.log(path.join(process.cwd(), '/grunt-tasks'));
+
   loadGruntConfig(grunt, {
-    configPath: path.join(process.cwd(), 'grunt-tasks'),
+    configPath: path.join(process.cwd(), '/grunt-tasks'),
     data: projectConfig,
   });
 };
