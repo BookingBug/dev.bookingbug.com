10cbf428b6e867c63a2106f34caa3f05da76eb85
Max McKenzie <max@lemondigits.com>
2016-04-13 10:41:59 +0100

    linted gruntfile in line with style guide

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,29 @@
+'use strict';
+
 const path = require('path');
-const LoadGruntConfig = require('load-grunt-config');
+const loadGruntConfig = require('load-grunt-config');
 const execSync = require('child_process').execSync;
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   const environment = (process.env.LD_ENVIRONMENT || 'local');
 
-  const localGitHash = null;
+  let localGitHash = null;
   try {
     localGitHash = execSync('git rev-parse HEAD').toString().replace(/(\r\n|\n|\r)/gm, '');
   } catch (e) {
     return;
   }
 
-  const project_config = {};
-  project_config.__versionString = (
+  const projectConfig = {};
+  projectConfig.__versionString = (
     process.env.CI_BUILD_REF || localGitHash || 'ffffff'
   ).substr(0, 6);
-  project_config.environment = environment;
+  projectConfig.environment = environment;
 
   require('time-grunt')(grunt);
 
-  LoadGruntConfig(grunt, {
+  loadGruntConfig(grunt, {
     configPath: path.join(process.cwd(), 'grunt-tasks'),
-    data: project_config,
+    data: projectConfig,
   });
 };
