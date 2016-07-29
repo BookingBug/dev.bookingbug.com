d2ace68a99775290fab7b846981bc45861c6e8b2
Max McKenzie <max@lemondigits.com>
2016-07-29 14:27:42 +0100

    removed call to non-function

--- app/index.js
+++ app/index.js
@@ -21,9 +21,9 @@ const app = express();
 app.use(raven.middleware.express.requestHandler('https://729e7cc359a747b3bf892b92f2d307ca:6e3e509a59034a56a3c0a9581791b0df@app.getsentry.com/85688'));
 app.use(raven.middleware.express.errorHandler('https://729e7cc359a747b3bf892b92f2d307ca:6e3e509a59034a56a3c0a9581791b0df@app.getsentry.com/85688'));
 
-raven.config({
-  environment: process.env.NODE_ENV,
-});
+// raven.config({
+//   environment: process.env.NODE_ENV,
+// });
 
 app.set('views', `${process.cwd} views`);
 app.set('view engine', 'twig');
